8458432c7a0e8e7865f0984a1ae7aa943d7f4b9c
rahul kirpekar <rahul.kirpekar@gmail.com>
2024-08-20 19:05:07 +0530

    b1

--- src/oops/vartype/Student.java
+++ src/oops/vartype/Student.java
@@ -4,6 +4,20 @@ public class Student
 {
 	// Data Members----->Instance Variables
 	int rno;
-	String name=new String();
+	String name;
 	int std;
+	
+	// Default Constructor
+	Student()
+	{
+		System.out.println("START :: Default Constructor---"+this);
+//		System.out.println(rno +" " + name+" "+std ) ;
+		
+		rno=1;
+		name="rahul";
+		std=12;
+
+//		System.out.println(rno +" " + name+" "+std ) ;
+		System.out.println("EXIT :: Default Constructor");
+	}
 }
--- src/oops/vartype/TestApp1.java
+++ src/oops/vartype/TestApp1.java
@@ -5,11 +5,13 @@ public class TestApp1
 	public static void main(String[] args) 
 	{
 		// JVM - Default Construtcor
-		Student s= new Student();
-		
-		System.out.println("s.rno : " + s.rno);
-		System.out.println("s.name : " + s.name);
-		System.out.println("s.std : " + s.std);
+		Student s1= new Student();
+		Student s2= new Student();
+		Student s3= new Student();
+
+		System.out.println(s1.rno+" " + s1.name+" " + s1.std+"----"+s1);
+		System.out.println(s2.rno+" " + s2.name+" " + s2.std+"----"+s2);
+		System.out.println(s3.rno+" " + s3.name+" " + s3.std+"----"+s3);
 		
 		// local variable
 //		int no1,no2,ans;
